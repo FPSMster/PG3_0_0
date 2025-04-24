33295cbb0882e69d48334970c5732b157794d4e3
GC1C_03_クドウ_マコト <k024g1039@g.neec.ac.jp>
2025-04-24 14:40:58 +0900

    1_1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include"stdio.h"
 
+template<typename T,typename T2>
+T Min(T a, T2 b) {
+	if (a<b){
+		return a;
+	}
+	if (a > b) {
+		return b;
+	}
+}
+
+template<>
+char Min<char>(char a, char b) {
+	printf("数字以外は代入できません");
+	return 0;
+}
+
 int main() {
 
-	printf("HelloPG3 00_01工藤マコト");
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%lf\n", Min<double>(11.4, 51.4));
+	printf("%c\n", Min<char>('a', 'b'));
 
 	return 0;
 }
